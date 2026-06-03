6fc81b2b68c0aa84fb2b3c0f8dd276de042b21cb
Florian Rieder <florian.rieder@unil.ch>
2026-06-03 23:24:56 +0200

    feat(terminal): add scroll to bottom

--- src/js/terminal.js
+++ src/js/terminal.js
@@ -1,11 +1,18 @@
 const output = document.getElementById("console-output");
+const container = document.getElementById("console");
+
+function scrollToBottom() {
+  container.scrollTop = container.scrollHeight;
+}
 
 export const Terminal = Object.freeze({
   print(message) {
     output.textContent += message;
+    scrollToBottom();
   },
   printLine(message) {
     output.textContent += message + "\n";
+    scrollToBottom();
   },
   clear() {
     output.textContent = "";
